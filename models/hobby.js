60c8a8672286cd9b41ca51fa52b06792d9a288b4
ella1220 <ellago1220@gmail.com>
2025-06-26 17:03:35 +0300

    Update hobby model

--- models/hobby.js
+++ models/hobby.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const Hobby = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    practiceTime: {
+        type: Number,
+        required: true
+    },
+    maxParticipants: {
+        type: Number,
+        required: true
+    },
+
+});
+
+module.exports = mongoose.model('Hobby', Hobby);
