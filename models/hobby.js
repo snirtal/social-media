3de1fa87691b3d6015fdd1ffc32e60c36517c957
snirtal <snirtal1122@gmail.com>
2025-07-20 15:47:29 +0300

    Add More Properties To Hobby Model

--- models/hobby.js
+++ models/hobby.js
@@ -10,19 +10,27 @@ const Hobby = new Schema({
     type: String,
     required: true
   },
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    practiceTime: {
-        type: Number,
-        required: true
-    },
-    maxParticipants: {
-        type: Number,
-        required: true
-    },
-
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  practiceTime: {
+    type: Number,
+    required: true
+  },
+  maxParticipants: {
+    type: Number,
+    required: true
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  createdBy: {
+    type: Schema.Types.ObjectId, 
+    ref: 'User',                 
+    required: false               
+  }
 });
 
 module.exports = mongoose.model('Hobby', Hobby);
