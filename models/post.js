aad8a219526881c2ab4732b00ece9fa4c4380aa1
snirtal <snirtal1122@gmail.com>
2025-07-20 15:29:21 +0300

    Add Likes To Post Model

--- models/post.js
+++ models/post.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema;
 const Post = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true },
-  title: { type: String, required: true },
-  created: { type: Date, default: Date.now }
+  created: { type: Date, default: Date.now },
+  imagePath: {type: String, default: '', required: false},
+  likes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 });
 
 module.exports = mongoose.model('Post', Post);
